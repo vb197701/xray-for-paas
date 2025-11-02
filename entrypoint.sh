a0629cfde7a4ec276fe9822fdeb824e068f3cf0c
VictorB <vb197701@gmail.com>
2025-11-02 15:22:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ sed -i "s#RELEASE_RANDOMNESS#${RELEASE_RANDOMNESS}#g" /etc/supervisor/conf.d/sup
 
 # 设置 nginx 伪装站
 rm -rf /usr/share/nginx/*
-wget https://gitlab.com/Misaka-blog/xray-paas/-/raw/main/mikutap.zip -O /usr/share/nginx/mikutap.zip
+wget https://github.com/vb197701/xray-for-paas/raw/main/mikutap.zip -O /usr/share/nginx/mikutap.zip
 unzip -o "/usr/share/nginx/mikutap.zip" -d /usr/share/nginx/html
 rm -f /usr/share/nginx/mikutap.zip
 
